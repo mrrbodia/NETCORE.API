132cfa32450e80e7dd3cf20ea248b7b63ece1602
mrrbodia <02bodia20@ukr.net>
2018-11-04 14:27:14 +0200

    setup project

---
+++ NETCore.API/Controllers/CitiesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NETCore.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class CitiesController : Controller
+    {
+        [HttpGet]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(new List<object>() {
+                new { id = 0, Name = "Zhytomyr" },
+                new { id = 1, Name = "Kyiv" }
+            });
+        }
+    }
+}
