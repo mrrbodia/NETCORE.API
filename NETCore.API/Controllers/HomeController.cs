653b222f52b092253554da56c4accc8d4c44f515
mrrbodia <02bodia20@ukr.net>
2018-11-21 00:05:37 +0200

    entities and migrations

--- NETCore.API/Controllers/HomeController.cs
+++ NETCore.API/Controllers/HomeController.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using NETCore.API.Entities;
 
 namespace NETCore.API.Controllers
 {
     [Route("shop")]
     public class HomeController : Controller
     {
+        private CityInfoContext cityInfoContext;
+
+        public HomeController(CityInfoContext context)
+        {
+            this.cityInfoContext = context; 
+        }
+
         public IActionResult Index()
         {
             return View();
--- NETCore.API/Startup.cs
+++ NETCore.API/Startup.cs
@@ -9,6 +9,9 @@ using Microsoft.Extensions.DependencyInjection;
 using JavaScriptEngineSwitcher.Core;
 using JavaScriptEngineSwitcher.ChakraCore;
 using React.AspNet;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using NETCore.API.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace NETCore.API
 {
@@ -18,9 +21,14 @@ namespace NETCore.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc()
+                .AddMvcOptions(o => o.OutputFormatters
+                    .Add(new XmlDataContractSerializerOutputFormatter()));
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddReact();
+
+            var connectionString = @"Server=(localdb)\MSSQLLocalDB;Database=CityInfo;Trusted_Connection=True;";
+            services.AddDbContext<CityInfoContext>(o => o.UseSqlServer(connectionString));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
